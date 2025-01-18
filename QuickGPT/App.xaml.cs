dc5542de30db53212c9a73a3d4e83735ed550d3d
silentesc <116022641+silentesc@users.noreply.github.com>
2025-01-18 11:23:50 +0100

    Add project files.

---
+++ QuickGPT/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace QuickGPT
+{
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            ShutdownMode = ShutdownMode.OnExplicitShutdown;
+
+            // Wait for keyboard shortcut
+
+            PromtWindow promtWindow = new();
+            promtWindow.Show();
+        }
+    }
+
+}
---
+++ QuickGPT/PromtWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+
+namespace QuickGPT
+{
+    public partial class PromtWindow : Window
+    {
+        public PromtWindow()
+        {
+            InitializeComponent();
+
+            TextBoxPromt.Focus();
+        }
+
+        private void TextBoxPromt_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (e.Key != System.Windows.Input.Key.Enter)
+            {
+                return;
+            }
+
+            // TODO Open normal chat window with request
+        }
+
+        private void Window_LostKeyboardFocus(object sender, System.Windows.Input.KeyboardFocusChangedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
